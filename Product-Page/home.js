59d84721c542baab789e198e7bba5358cb021c5e
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2023-12-21 21:57:53 +0200

    firebase installition

--- Product-Page/home.js
+++ Product-Page/home.js
@@ -33,23 +33,23 @@ for (let i = 0; i < dropList.length; i++) {
 
 let discountDate = new Date("Dec 25 2023 9:00:00").getTime();
 
-let countDown = function () {
-  setInterval(function () {
-    let currentDate = new Date();
-    let coolDown = discountDate - currentDate;
+// let countDown = function () {
+//   setInterval(function () {
+//     let currentDate = new Date();
+//     let coolDown = discountDate - currentDate;
 
-    let days = Math.floor(coolDown / (1000 * 60 * 60 * 24));
-    let hours = Math.floor(
-      (coolDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    let minutes = Math.floor((coolDown % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((coolDown % (1000 * 60)) / 1000);
+//     let days = Math.floor(coolDown / (1000 * 60 * 60 * 24));
+//     let hours = Math.floor(
+//       (coolDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//     );
+//     let minutes = Math.floor((coolDown % (1000 * 60 * 60)) / (1000 * 60));
+//     let seconds = Math.floor((coolDown % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("min").innerHTML = minutes;
-    document.getElementById("sec").innerHTML = seconds;
-  }, 1000);
-};
+//     document.getElementById("days").innerHTML = days;
+//     document.getElementById("hours").innerHTML = hours;
+//     document.getElementById("min").innerHTML = minutes;
+//     document.getElementById("sec").innerHTML = seconds;
+//   }, 1000);
+// };
 
-countDown();
+// countDown();
