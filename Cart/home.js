6a6edc164f0712e6a736226ed17513997581ae4e
Amraboelmagdhussien <120594701+Amraboelmagdhussien@users.noreply.github.com>
2023-12-22 21:21:49 +0200

    Created Cart Page

---
+++ Cart/home.js
@@ -0,0 +1,11 @@
+let dropList = document.querySelectorAll(".dropDownList");
+for (let i = 0; i < dropList.length; i++) {
+  let list = dropList[i];
+  list.addEventListener("mouseover", function () {
+    list.classList.add("showList");
+  });
+
+  list.addEventListener("mouseleave", function () {
+    list.classList.remove("showList");
+  });
+}
