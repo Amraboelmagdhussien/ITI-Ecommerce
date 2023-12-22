b2d0744fb20149282a16ef3437a543d04a22b6e4
Amraboelmagdhussien <amraboelmagdhussien@gmail.com>
2023-12-22 13:52:16 -0800

    Testing Done

--- Product-Page/home.js
+++ Product-Page/home.js
@@ -10,10 +10,28 @@ for (let i = 0; i < dropList.length; i++) {
   });
 }
 
-let arrProd = [];
 
- let pushFunc = function() {
+document.addEventListener('DOMContentLoaded', function() {
+  let arrProd = [];
 
+ let products = document.querySelectorAll(".products")
+ let prod = document.querySelectorAll(".product-card-1")
+ let pushButton = document.getElementById("prodButton")
+ 
+
+
+ let add = function() {
+  let i = 0
+  for (let i = 0; i < prod.length; i++) {
+    arrProd.push(prod[i]);
+    // console.log(prod[i]);
+   }
+  return arrProd.push(prod[i])
+  
  }
-let test = document.getElementById("prodButton")
-console.log(test);
+
+ console.log(add());
+
+console.log(arrProd);
+});
+
