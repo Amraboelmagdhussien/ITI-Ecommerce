d8ffa6d9590bddf12a37fd3e0b631d007561cdec
Amraboelmagdhussien <120594701+Amraboelmagdhussien@users.noreply.github.com>
2023-12-23 02:34:32 +0200

    Fix

--- Product-Page/home.js
+++ Product-Page/home.js
@@ -9,30 +9,3 @@ for (let i = 0; i < dropList.length; i++) {
     list.classList.remove("showList");
   });
 }
-
-
-
-  let arrProd = [];
-
- let products = document.querySelectorAll(".products")
- let prod = document.querySelectorAll(".product-card-1")
- let pushButton = document.getElementById("prodButton")
- 
-
-
-
-
- let add = function(button) {
-   // Get the parent div with class 'product-card-1' from the clicked button
-   let currentProduct = document.querySelector(".product-card-1");
-   let nameProduct = document.querySelector(".data-name")
-   console.log(nameProduct.innerHTML);
-   if (currentProduct) {
-     arrProd.push(currentProduct);
-     console.log(currentProduct.title);
-   } else {
-     console.error("Product card not found");
-   }
- };
-
-
