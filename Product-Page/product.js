d7c75d2b2bfb40b42e93db4b5d450dfa9c6df33a
Amraboelmagdhussien <120594701+Amraboelmagdhussien@users.noreply.github.com>
2023-12-24 21:28:29 +0200

    Updated LocalStorage

--- Product-Page/product.js
+++ Product-Page/product.js
@@ -1,61 +1,67 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
- import {
-   getDatabase,
-   ref,
-   set,
-   get,
-   child,
-   onValue,
- } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  child,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
- // https://firebase.google.com/docs/web/setup#available-libraries
- // Your web app's Firebase configuration
- const firebaseConfig = {
-   apiKey: "AIzaSyADIzVJeauTcF1WjKGMipnNNH3cu8PagDg",
-   authDomain: "gothic-album-381415.firebaseapp.com",
-   databaseURL: "https://gothic-album-381415-default-rtdb.firebaseio.com",
-   projectId: "gothic-album-381415",
-   storageBucket: "gothic-album-381415.appspot.com",
-   messagingSenderId: "474429480537",
-   appId: "1:474429480537:web:5b32b38a9a22072f965b48",
-   measurementId: "G-5TEVJS063P",
- };
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADIzVJeauTcF1WjKGMipnNNH3cu8PagDg",
+  authDomain: "gothic-album-381415.firebaseapp.com",
+  databaseURL: "https://gothic-album-381415-default-rtdb.firebaseio.com",
+  projectId: "gothic-album-381415",
+  storageBucket: "gothic-album-381415.appspot.com",
+  messagingSenderId: "474429480537",
+  appId: "1:474429480537:web:5b32b38a9a22072f965b48",
+  measurementId: "G-5TEVJS063P",
+};
 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- if (app) {
-   console.log("done Worked!!");
- }
- const db = getDatabase();
- const dbRef = ref(getDatabase());
- let cartItems = [];
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+if (app) {
+  console.log("done Worked!!");
+}
+const db = getDatabase();
+const dbRef = ref(getDatabase());
+let cartItems = [];
 
- let addToCart = async function (id) {
-   const snapshot = await get(child(dbRef, id)); // btrg3 promise
-   const prod = snapshot.val();
-   cartItems.push(prod);
-   localStorage.setItem("cartItems", JSON.stringify(cartItems));
- };
- console.log(cartItems);
- // read Data from firebse
+let localItems = localStorage.getItem("allproduct");
+let parsedItem = JSON.parse(localItems || "[]");
 
- const postId = 1;
- const starCountRef = ref(db, "posts/" + postId + "/starCount");
- // onValue(starCountRef, (snapshot) => {
- //   const data = snapshot.val();
- //   updateStarCount(postElement, data);
- //   // console.log(data)
- // });
+console.log(parsedItem);
 
- for (let i = 0; i < 20; i++) {
-   const userId = i;
-   get(child(dbRef, `/${userId}`))
-     .then((snapshot) => {
-       if (snapshot.exists()) {
-         var Data = snapshot.val();
-         const prodEle = document.createElement("div");
-         prodEle.innerHTML = ` <div class="product-card-1">
+let addToCart = async function (id) {
+  const snapshot = await get(child(dbRef, id)); // btrg3 promise
+  const prod = snapshot.val();
+  cartItems.push(parsedItem);
+  cartItems.push(prod);
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+};
+console.log(cartItems);
+// read Data from firebse
+
+const postId = 1;
+const starCountRef = ref(db, "posts/" + postId + "/starCount");
+// onValue(starCountRef, (snapshot) => {
+//   const data = snapshot.val();
+//   updateStarCount(postElement, data);
+//   // console.log(data)
+// });
+
+for (let i = 0; i < 20; i++) {
+  const userId = i;
+  get(child(dbRef, `/${userId}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        var Data = snapshot.val();
+        const prodEle = document.createElement("div");
+        prodEle.innerHTML = ` <div class="product-card-1">
              <div class="product-image-1">
                <img src="${Data.image}"   alt="" />
              </div>
@@ -73,23 +79,19 @@
              </div>
            </div>
  `;
-         prodEle
-           .querySelector("button")
-           .addEventListener("click", async () => {
-             await addToCart(userId + "");
-             console.log(cartItems);
-           });
-         document.getElementById("Data").appendChild(prodEle);
-       } else {
-         console.log("No data available");
-       }
-     })
-     .catch((error) => {
-       console.log(error);
-     });
- }
-
-
+        prodEle.querySelector("button").addEventListener("click", async () => {
+          await addToCart(userId + "");
+          console.log(cartItems);
+        });
+        document.getElementById("Data").appendChild(prodEle);
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 let dropList = document.querySelectorAll(".dropDownList");
 for (let i = 0; i < dropList.length; i++) {
@@ -103,14 +105,11 @@ for (let i = 0; i < dropList.length; i++) {
   });
 }
 
-// display 
-function display_items(){ 
-  var productContainer = JSON.parse(localStorage.getItem('allproduct'))
-  for(var i =0 ; i<productContainer.length;i++){
-   document.getElementById(
-         "Data"
-                ).innerHTML += 
-                    `
+// display
+function display_items() {
+  var productContainer = JSON.parse(localStorage.getItem("allproduct"));
+  for (var i = 0; i < productContainer.length; i++) {
+    document.getElementById("Data").innerHTML += `
                     <div class="product-card-1">
                     <div class="product-image-1">
                       <img src="${productContainer[i].pimg}"   alt="" />
@@ -128,7 +127,6 @@ function display_items(){
                       </div>
                     </div>
                   <br>`;
-    }
   }
-  display_items();
-
+}
+display_items();
