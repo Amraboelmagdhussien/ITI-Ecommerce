30e7b39dc6681b33560459ce4b6affcb62fabc6f
Amraboelmagdhussien <120594701+Amraboelmagdhussien@users.noreply.github.com>
2023-12-25 03:51:30 +0200

    Added items to Homepage

--- home/home.js
+++ home/home.js
@@ -1,24 +1,68 @@
-// hover over elements
-// used this to method to target multiplie element
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  child,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// let hoverOver = function () {
-//   let hoverElements = document.getElementsByClassName("drop-down");
-//   let elements = document.getElementsByClassName("test");
-//   for (let i = 0; i < hoverElements.length; i++) {
-//     hoverElements[i].classList.remove("drop-down");
-//     hoverElements[i].classList.add("showList");
-//   }
-// };
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADIzVJeauTcF1WjKGMipnNNH3cu8PagDg",
+  authDomain: "gothic-album-381415.firebaseapp.com",
+  databaseURL: "https://gothic-album-381415-default-rtdb.firebaseio.com",
+  projectId: "gothic-album-381415",
+  storageBucket: "gothic-album-381415.appspot.com",
+  messagingSenderId: "474429480537",
+  appId: "1:474429480537:web:5b32b38a9a22072f965b48",
+  measurementId: "G-5TEVJS063P",
+};
 
-// let hideElement = function () {
-//   let hoverElements = document.getElementsByClassName("drop-down");
-//   let elements = document.getElementsByClassName("sub-cate");
-//   for (let i = 0; i < hoverElements.length; i++) {
-//     hoverElements[i].classList.remove("showList");
-//     elements[i].classList.add("drop-down");
-//   }
-// };
-// hi amr 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+if (app) {
+  console.log("done Worked!!");
+}
+const db = getDatabase();
+const dbRef = ref(getDatabase());
+
+const postId = 1;
+const starCountRef = ref(db, "posts/" + postId + "/starCount");
+
+for (let i = 0; i < 8; i++) {
+  const userId = i;
+  get(child(dbRef, `/${userId}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        var Data = snapshot.val();
+        console.log(Data);
+        document.getElementById(
+          "grid-container"
+        ).innerHTML += `<div class="grid-item grid-item-2">
+        <div class="productsInformation">
+          <h4>${Data.category}</h4>
+          <p class="price-info">
+            From <br />
+            <span class="price">${Data.price} USD </span>
+          </p>
+        </div>
+
+        <div class="imgProducts">
+          <img class="imagesss" src="${Data.image}" alt="" />
+        </div>
+      </div>`;
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 let dropList = document.querySelectorAll(".dropDownList");
 for (let i = 0; i < dropList.length; i++) {
   let list = dropList[i];
